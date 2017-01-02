b7a6e59d3200fcf5cdd292efc1434dd6f8fb0172
rotsee <leo_wallentin@hotmail.com>
2017-01-02 19:57:47 +0100

    full menu, lead

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,28 @@ router.get('/', function(req, res, next) {
 
   var defaults = {
     software: {
-      allowed: ["Excel", "Google Sheets", "LibreOffice/OpenOffice"],
-      default: "Excel",
+      allowed: ["Excel for Windows", "Excel for Mac", "Google Sheets", "LibreOffice/OpenOffice", "NeoOffice"],
+      default: "Excel for Window",
+    },
+    language: {
+      allowed: ["English", "Swedish"],
+      default: "English"
+    },
+    locale: {
+      allowed: ["sv-SE", "en-US", "en-GB"],
+      default: "en-US"
     }
+  }
+  var versions = {
+    "Excel for Windows": ["Excel 2016", "Excel 2013", "Excel 2010", "Excel 2007"], 
+    "Excel for Mac": ["Excel 2016", "Excel 2015", "Excel 2011", "Excel 2008"],
+    "LibreOffice/OpenOffice": ["5.0.0", "4.4.0", "4.3.0", "4.2.0", "4.1.0", "3.6.0"],
+    "Google Sheets": [],
+    "NeoOffice": ["NeoOffice 2015", "NeoOffice 2014", "NeoOffice 2013", "NeoOffice 3.4"]
+  }
+
+  var chapters = {
+    "What software should I use?": "software-intro"
   }
 
   for (var default_ in defaults){
@@ -19,15 +38,29 @@ router.get('/', function(req, res, next) {
     }
   }
 
+  if (defaults.software.selected == "Excel for Windows") {
+    var activeOS = "Windows"
+  } else if (defaults.software.selected == "Excel for Mac") {
+    var activeOS = "MacOS"
+  } else if (defaults.software.selected == "NeoOffice") {
+    var activeOS = "MacOS"
+  } else {
+    var activeOS = null
+  }
+
 
   res.render('index', {
     lang: "en",
     availableSoftwares: defaults.software.allowed,
     activeSoftware: defaults.software.selected,
-    os: "Windows",
-    ver: 16,
-    softwareLang: "sv",
-    spreadsheetLang: "sv"  // mostly relevant for Google Sheets
+    activeOS: activeOS,
+    availableVersions: versions[defaults.software.selected],
+    activeVersion: versions[defaults.software.selected].selected,
+    availableLanguages: defaults.language.allowed,
+    activeLanguage: defaults.language.selected,
+    availableLocales: defaults.locale.allowed,
+    activeLocale: defaults.locale.selected,
+    chapters: chapters,
   })
 
 })
