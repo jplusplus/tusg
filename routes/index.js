7db12c2e36839acf57dd2bea4ceded1de3d7787f
rotsee <leo_wallentin@hotmail.com>
2017-01-02 10:36:06 +0100

    parameter handling

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,26 @@ var textFunctions = require("../lib/text-functions.js")("sv-SE")
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
+  var defaults = {
+    software: {
+      allowed: ["Excel", "Google Sheets", "LibreOffice/OpenOffice"],
+      default: "Excel",
+    }
+  }
+
+  for (var default_ in defaults){
+    if (req.query[default_] && (defaults[default_].allowed.indexOf(req.query[default_]) > -1)){
+      defaults[default_].selected = req.query[default_]
+    } else {
+      defaults[default_].selected = defaults[default_].default
+    }
+  }
+
+
   res.render('index', {
     lang: "en",
-    software: "Excel",
+    availableSoftwares: defaults.software.allowed,
+    activeSoftware: defaults.software.selected,
     os: "Windows",
     ver: 16,
     softwareLang: "sv",
