6866c32154530647cbc16a724e32a389ad279e98
rotsee <leo_wallentin@hotmail.com>
2017-01-18 22:47:04 +0100

    style

--- resources/js/main.js
+++ resources/js/main.js
@@ -18,23 +18,18 @@ function updateURL(key, val){
   var url = window.location.href;
   var reExp = new RegExp("[\?|\&]"+key + "=[0-9a-zA-Z\_\+\-\|\.\,\;]*");
 
-if(reExp.test(url))
-{ // update
-    reExp = new RegExp("[\?&]" + key + "=([^&#]*)");
-    var delimiter = reExp.exec(url)[0].charAt(0);
-    url = url.replace(reExp, delimiter + key + "=" + val);
-}
-else
-{ // add
+  if(reExp.test(url)){
+      reExp = new RegExp("[\?&]" + key + "=([^&#]*)");
+      var delimiter = reExp.exec(url)[0].charAt(0);
+      url = url.replace(reExp, delimiter + key + "=" + val);
+  } else {
     var newParam = key + "=" + val;
     if(url.indexOf('?') === -1){url += '?';}
 
     if(url.indexOf('#') > -1){
         var urlparts = url.split('#');
         url = urlparts[0] +  "&" + newParam +  (urlparts[1] ?  "#" +urlparts[1] : '');
-    }
-    else
-    {
+    } else {
         url += "&" + newParam;
     }
   }
